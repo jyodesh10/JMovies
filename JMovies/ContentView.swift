70998c7a3c045cc5c739d114a60f11d0f2ce38cb
jyodesh10 <jyodeshshakya@gmail.com>
2026-03-31 17:11:24 +0200

    Initial Commit

---
+++ JMovies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JMovies
+//
+//  Created by Jyodesh Shakya on 31.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JMovies/JMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JMoviesApp.swift
+//  JMovies
+//
+//  Created by Jyodesh Shakya on 31.03.26.
+//
+
+import SwiftUI
+
+@main
+struct JMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
