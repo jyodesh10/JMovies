e35b185a919b4b39da9df45c451f6673d2d82835
jyodesh10 <jyodeshshakya@gmail.com>
2026-07-10 03:11:18 +0200

    added lsearch feature, local data storage with SwiftData

---
+++ JMovies/DownloadView.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadView.swift
+//  JMovies
+//
+//  Created by Jyodesh Shakya on 10.07.26.
+//
+
+import SwiftUI
+
+struct DownloadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadView()
+}
---
+++ JMovies/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  JMovies
+//
+//  Created by Jyodesh Shakya on 10.07.26.
+//
+
+import Foundation
